0b8633966b3ec5686274c920713a27a047f68aa8
ClassyDragon <cgandeg@gmail.com>
2019-10-10 10:48:42 -0400

    modified:   Block.cpp 	modified:   Block.h

--- src/Block.cpp
+++ src/Block.cpp
@@ -7,16 +7,20 @@ Block::Block() {
 Block::~Block() {
 }
 
+void Block::render(sf::RenderWindow* window) {
+    window->draw(sprite);
+}
+
 void Block::setTexture(sf::Texture* texture) {
     this->texture = texture;
     sprite.setTexture(this->texture);
     sprite.setSize(static_cast<sf::Vector2f>(texture->getSize()));
 }
 
-void Block::setPosition(sf::Vector2f pos) {
+void Block::set_screen_position(sf::Vector2f pos) {
     sprite.setPosition(pos);
 }
 
-void Block::render(sf::RenderWindow* window) {
-    window->draw(sprite);
+void Block::set_field_position(sf::Vector2f field_position) {
+    this->field_position = field_position;
 }
--- src/Block.h
+++ src/Block.h
@@ -6,12 +6,23 @@
 
 class Block {
     public:
+        // Default Constructor:
         Block();
         ~Block();
-        void setTexture(sf::Texture* texture);
-        void render(sf::RenderWindow* window);
-        void setPosition(sf::Vector2f pos);
+        // Display the block to the window:
+        void render(sf::RenderWindow* window); 
+        // Set Texture of block:
+        void setTexture(sf::Texture* texture); // from std::map textures in Field object
+        // Set Screen Position:
+        void set_screen_position(sf::Vector2f pos);
+        // Set Field Position:
+        void set_field_position(sf::Vector2f field_position);
     private:
+        // Sprite:
         sf::RectangleShape sprite;
         sf::Texture* texture;
+        // Coordinate Position on Screen:
+        sf::Vector2f screen_position;
+        // Field Position:
+        sf::Vector2f field_position;
 };
