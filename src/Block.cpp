9628c69ac1802e550457acb998ac61d14237d7ad
ClassyDragon <cgandeg@gmail.com>
2019-12-18 18:47:13 -0500

    Removed unused functions, changed return type of getFieldPosition()

--- src/Block.cpp
+++ src/Block.cpp
@@ -1,70 +1,57 @@
 #include "Block.h"
 
+// Default Constructor:
 Block::Block() {
     solid = false;
 }
 
-Block::~Block() {
-}
-
+// Draw block to window:
 void Block::render(sf::RenderWindow* window) {
     window->draw(sprite);
 }
 
+// Set the texture of the block:
 void Block::setTexture(sf::Texture* texture) {
     this->texture = texture;
     sprite.setTexture(texture);
     sprite.setSize(static_cast<sf::Vector2f>(texture->getSize()));
 }
 
+// Return the address of the block's texture:
 sf::Texture* Block::getTexture() {
     return texture;
 }
 
+// Set the position of the block on the window:
 void Block::setScreenPosition(sf::Vector2f pos) {
     sprite.setPosition(pos);
 }
 
-void Block::setFieldPosition(sf::Vector2f fieldPosition) {
+// Set the Vector2i representing the field position:
+void Block::setFieldPosition(sf::Vector2i fieldPosition) {
     this->fieldPosition = fieldPosition;
 }
 
+// Get the current screen position of the block:
 sf::Vector2f Block::getScreenPosition() {
     return screenPosition;
 }
 
-sf::Vector2f Block::getFieldPosition() {
+// Get the current field position of the block:
+sf::Vector2i Block::getFieldPosition() {
     return fieldPosition;
 }
 
-bool Block::canMoveLeft() {
-    if (fieldPosition.x - 1 < 0)
-        return false;
-    return true;
-}
-
-bool Block::canMoveRight() {
-    if (fieldPosition.x + 1 >= field_width_v)
-        return false;
-    return true;
-}
-
 void Block::moveLeft() {
-    fieldPosition = sf::Vector2f(fieldPosition.x - 1, fieldPosition.y);
+    fieldPosition = sf::Vector2i(fieldPosition.x - 1, fieldPosition.y);
 }
 
 void Block::moveRight() {
-    fieldPosition = sf::Vector2f(fieldPosition.x + 1, fieldPosition.y);
-}
-
-bool Block::canMoveDown() {
-    if (fieldPosition.y + 1 >= field_height_v)
-        return false;
-    return true;
+    fieldPosition = sf::Vector2i(fieldPosition.x + 1, fieldPosition.y);
 }
 
 void Block::moveDown() {
-    fieldPosition = sf::Vector2f(fieldPosition.x, fieldPosition.y + 1);
+    fieldPosition = sf::Vector2i(fieldPosition.x, fieldPosition.y + 1);
 }
 
 void Block::moveScreenPosition(sf::Vector2f offset) {
